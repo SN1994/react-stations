923ac504a8718537ea23f381c6bfb418027a245f
SN1994 <night-carnival1219@docomo.ne.jp>
2021-12-09 22:06:56 +0900

    react4 コミット

--- src/App.js
+++ src/App.js
@@ -7,27 +7,21 @@ import './App.css'
  * @type {React.FC}
  */
 
- function Dog() {
 
+
+export const App = () => {
   const [dogUrl, setDogUrl] 
   = useState(<img src="https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_1866.jpg" />);
 
-  return (
+    return (
       <div>
+        <header> 
+        <h1>Orginal Dogアプリ</h1>
+      </header>
+      <p>犬の画像紹介サイトです！！</p>
         { dogUrl }
         { setDogUrl }
       </div>
 );
 }
 
-export const App = () => {
-    return ( 
-    <div>
-      <header> 
-        <h1>Orginal Dogアプリ</h1>
-      </header>
-          <p>犬の画像紹介サイトです！！</p>
-          <img src="https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_1866.jpg" />
-    </div>
-    );
-}
