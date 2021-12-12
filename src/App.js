58ee310cd6982c8255e68a7230eb534deac9d728
SN1994 <night-carnival1219@docomo.ne.jp>
2021-12-12 10:54:20 +0900

    react5 コミット

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css'
  * 
  * @type {React.FC}
  */
-
 export const App = () => {
   const [dogUrl, setDogUrl] 
   = useState("https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_1866.jpg");
@@ -18,7 +17,10 @@ export const App = () => {
       </header>
       <p>犬の画像紹介サイトです！！</p>
       <img src = {dogUrl} />
+      <p><button onClick={() => 
+        setDogUrl("https://images.dog.ceo/breeds/dachshund/dog-1018408_640.jpg")}>
+        Click me
+      </button></p>
       </div>
 );
 }
-
